3994c50329881d9e7f69c87c0d565244a20b36dd
Yunseul <117149045+yunseul-dev@users.noreply.github.com>
2024-06-13 23:34:03 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -0,0 +1,9 @@
+def solution(number, k):
+    stack = []
+    for num in number:
+        while k > 0 and stack and stack[-1] < num:
+            stack.pop()
+            k -= 1
+        stack.append(num)
+
+    return ''.join(stack[:len(number)-k])
