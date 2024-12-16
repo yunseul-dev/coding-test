c0d7851259e37dc17cae1887a4350386d768d7dd
Yunseul <117149045+yunseul-dev@users.noreply.github.com>
2024-12-16 14:42:17 +0900

    [Gold V] Title: LCS, Time: 148 ms, Memory: 18764 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String A = br.readLine();
+        String B = br.readLine();
+
+        int[][] dp = new int[A.length() + 1][B.length() + 1];
+
+        for (int i = 1; i <= A.length(); i++) {
+            for (int j = 1; j <= B.length(); j++) {
+                if (A.charAt(i - 1) == B.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        bw.write(String.valueOf(dp[A.length()][B.length()]));
+        bw.flush();
+        bw.close();
+    }
+}
