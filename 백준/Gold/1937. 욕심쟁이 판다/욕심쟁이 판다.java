65753be696ab0d42e57029bd58bc4d3aba84cf0f
Yunseul <117149045+yunseul-dev@users.noreply.github.com>
2025-01-22 17:40:17 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 468 ms, Memory: 37692 KB -BaekjoonHub

---
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int[][] trees;
+    static int[][] dp;
+    static int n;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, -1, 0, 1};
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        n = Integer.parseInt(br.readLine());
+        trees = new int[n][n];
+        dp = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                trees[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                max = Math.max(max, dfs(i, j));
+            }
+        }
+
+        bw.write(max + "\n");
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+
+    static int dfs(int x, int y) {
+        if (dp[x][y] != 0) {
+            return dp[x][y];
+        }
+
+        dp[x][y] = 1; 
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx >= 0 && nx < n && ny >= 0 && ny < n && trees[nx][ny] > trees[x][y]) {
+                dp[x][y] = Math.max(dp[x][y], dfs(nx, ny) + 1);
+            }
+        }
+
+        return dp[x][y];
+    }
+}
