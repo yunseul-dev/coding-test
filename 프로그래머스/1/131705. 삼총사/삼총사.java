e6dc366653f247efcd20b0c73137f9e5408a1c42
Yunseul <117149045+yunseul-dev@users.noreply.github.com>
2025-02-03 09:23:54 +0900

    [level 1] Title: 삼총사, Time: 0.04 ms, Memory: 84.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int[] number) {
+        int answer = 0;
+
+        int n = number.length;
+        
+        for(int i = 0; i < n - 2; i++){
+            for(int j = i + 1; j < n - 1; j++){
+                for(int k = j + 1; k < n; k++){
+                    if(number[i] + number[j] + number[k] == 0){
+                        answer++;
+                    }
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
